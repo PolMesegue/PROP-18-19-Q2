4d5b03941492f40c44322c169877ce84c01d6c71
Pol Mesegue <pol.mesegue@est.fib.upc.edu>
2019-03-25 08:54:23 +0100

    driverPeça

---
+++ src/DriverPeca.java
@@ -0,0 +1,19 @@
+
+
+
+
+
+class DriverPeça {
+
+    private static Peça p;
+
+    public void testConstructora();
+    public void testesBlanca();
+    public void testesNegra();
+    public void testsetX();
+    public void testsetY();
+    public void testgetX();
+    public void testgetY();
+    public void testmoviment_valid();
+
+}
