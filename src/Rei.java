1a9eddee6b3865c01e2bad8d59092a57987b2178
Jaume Riera Caba <33702960+Cabahamaru@users.noreply.github.com>
2019-04-03 18:38:52 +0200

    Rei casi done

--- src/Rei.java
+++ src/Rei.java
@@ -11,14 +11,30 @@ public class Rei extends Peca {
     }
 
     public ArrayList<IntPair> posibles_moviments(Tauler t) {
-
-
-        return null;
-
+        //el rei es pot moure a qualsevol casella que estigui a 1 de distancia
+        this.moviments.clear();
+        int posx[] = {this.getX() - 1, this.getX(), this.getX() + 1, this.getX() - 1, this.getX() + 1, this.getX() - 1, this.getX(), this.getX() + 1};
+        int posy[] = {this.getY() - 1, this.getY() - 1, this.getY() - 1, this.getY(), this.getY(), this.getY() + 1, this.getY() + 1, this.getY() + 1};
+        for (int i = 0; i < 8; ++i) {
+            if (posx[i] >= 0 && posx[i] < 8 && posy[i] >= 0 && posy[i] < 8) {
+                if (t.getPeca(posx[i],posy[i]) == null || t.getPeca(posx[i],posy[i]).getColor() != this.getColor()) {
+                    IntPair aux = new IntPair(posx[i], posy[i]);
+                    this.moviments.add(aux);
+                }
+            }
+        }
+        return this.moviments;
     }
 
-
+    //Funcio que mira si el Rei esta en perill
+    //Donat un estat del Tauler, mira si hi alguna peça de color contrari que pugui atacar al Rei
+    public boolean en_perill(Tauler t){
+        //en construccio
+        return false;
+    }
 }
+
+//IGNORAR A PARTI D'AQUI
     /*
     public boolean moviment_valid(Tauler t, int desti_x, int desti_y) {
         //El Rei es pot moure a qualsevol cel·la a distancia 1 de la seva posicio actual
