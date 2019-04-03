85516640c3283db7ee6c56688bb560f74c21ac09
Cabahamaru <jaume.riera200@gmail.com>
2019-04-03 21:43:47 +0200

    cavall done

--- src/Cavall.java
+++ src/Cavall.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-
+@SuppressWarnings("Duplicates")
 public class Cavall extends Peca{
 
 
@@ -11,11 +11,20 @@ public class Cavall extends Peca{
         setColor(color);
     }
 
-    public ArrayList<IntPair> posibles_moviments(Tauler t, int desti_x, int desti_y) {
-
-
-        return null;
-
+    public ArrayList<IntPair> posibles_moviments(Tauler t) {
+        //en qualsevol punt del cavall, hi ha maxim 8 punts on es pot moure
+        this.moviments.clear();
+        int posx[] = {this.getX() - 2, this.getX() - 1 , this.getX() + 1, this.getX() + 2, this.getX() - 2 , this.getX() - 1, this.getX() + 1, this.getX() - 2};
+        int posy[] = {this.getY() - 1, this.getY() - 2, this.getY() - 2, this.getY() - 1, this.getY() + 1, this.getY() + 2, this.getY() + 2, this.getY() + 1};
+        for (int i = 0; i < 8; ++i) {
+            if (posx[i] >= 0 && posx[i] < 8 && posy[i] >= 0 && posy[i] < 8) {
+                if (t.getPeca(posx[i],posy[i]) == null || t.getPeca(posx[i],posy[i]).getColor() != this.getColor()) {
+                    IntPair aux = new IntPair(posx[i],posy[i]);
+                    this.moviments.add(aux);
+                }
+            }
+        }
+        return this.moviments;
     }
 }
 
