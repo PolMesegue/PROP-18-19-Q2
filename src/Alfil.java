260a660d30247b89cc22edd00fba84a44f99294c
Cabahamaru <jaume.riera200@gmail.com>
2019-04-03 20:48:57 +0200

    alfil done

--- src/Alfil.java
+++ src/Alfil.java
@@ -15,7 +15,7 @@ public class Alfil extends Peca {
         //Un alfil es pot moure tantes cel·les com vulgui en les quatre digagonals.
         moviments.clear();
         int tempx = this.getX() + 1; //moviment
-        int tempy = this.getY() - 1; //diagonal dreta
+        int tempy = this.getY() - 1; //diagonal dreta-amunt
         while (tempx < 8 && tempy >= 0) {
             if (t.getPeca(tempx,tempy) == null) {
                 IntPair aux = new IntPair(tempx,tempy);
@@ -30,13 +30,62 @@ public class Alfil extends Peca {
             tempx++;
             tempy--;
         }
-        //el mateix per la resta de diagonals
-        //...
+
+        tempx = this.getX() - 1; //moviment
+        tempy = this.getY() + 1; //diagonal dreta-avall
+        while (tempx >= 0 && tempy < 8) {
+            if (t.getPeca(tempx,tempy) == null) {
+                IntPair aux = new IntPair(tempx,tempy);
+                this.moviments.add(aux);
+            }
+            else if (t.getPeca(tempx,tempy).getColor() == this.getColor()) break;
+            else {
+                IntPair aux = new IntPair(tempx,tempy);
+                this.moviments.add(aux);
+                break;
+            }
+            tempx--;
+            tempy++;
+        }
+
+        tempx = this.getX() - 1; //moviment
+        tempx = this.getY() - 1; //diagonal esquerra-amunt
+        while (tempx >= 0 && tempy >= 0) {
+            if (t.getPeca(tempx,tempy) == null) {
+                IntPair aux = new IntPair(tempx,tempy);
+                this.moviments.add(aux);
+            }
+            else if (t.getPeca(tempx,tempy).getColor() == this.getColor()) break;
+            else {
+                IntPair aux = new IntPair(tempx,tempy);
+                this.moviments.add(aux);
+                break;
+            }
+            tempx--;
+            tempy--;
+        }
+
+        tempx = this.getX() + 1; //moviment
+        tempx = this.getY() + 1; //diagonal esquerra-avall
+        while (tempx < 8 && tempy < 8) {
+            if (t.getPeca(tempx,tempy) == null) {
+                IntPair aux = new IntPair(tempx,tempy);
+                this.moviments.add(aux);
+            }
+            else if (t.getPeca(tempx,tempy).getColor() == this.getColor()) break;
+            else {
+                IntPair aux = new IntPair(tempx,tempy);
+                this.moviments.add(aux);
+                break;
+            }
+            tempx++;
+            tempy++;
+        }
         return this.moviments;
     }
 }
 
-//IGNORAR A APRTI D'AQUI
+//IGNORAR A PARTIR D'AQUI
 /*
     //Si hi ha una peça aliada a la posició destí, no ens podem moure
     Peça p_aux = t.getPeça(desti_x, desti_y);
