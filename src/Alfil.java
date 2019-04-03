04c96facec06407ca5b3eede48bf010d73fb8b98
Jaume Riera Caba <33702960+Cabahamaru@users.noreply.github.com>
2019-04-03 18:47:55 +0200

    alfil update

--- src/Alfil.java
+++ src/Alfil.java
@@ -1,30 +1,53 @@
-
+import java.util.*;
 
 
 public class Alfil extends Peca {
 
 
-    public Alfil(int x, int y, boolean es_blanca) {
-        super(x,y,es_blanca);
+    public Alfil(String i, int x, int y, boolean color) {
+        setId(i);
+        setX(x);
+        setY(y);
+        setColor(color);
     }
 
-    @Override
-    public boolean moviment_valid(Tauler t, int desti_x, int desti_y) {
+    public ArrayList<IntPair> posibles_moviments(Tauler t) {
         //Un alfil es pot moure tantes cel·les com vulgui en les quatre digagonals.
-
-        //Si hi ha una peça aliada a la posició destí, no ens podem moure
-        Peça p_aux = t.getPeça(desti_x, desti_y);
-
-        if (p_aux != null) {
-            if (p_aux.esBlanca() && this.esBlanca()) return false;
-            if (p_aux.esNegra() && this.esNegra()) return false;
+        moviments.clear();
+        int tempx = this.getX() + 1; //moviment
+        int tempy = this.getY() - 1; //diagonal dreta
+        while (tempx < 8 && tempy >= 0) {
+            if (t.getPeca(tempx,tempy) == null) {
+                IntPair aux = new IntPair(tempx,tempy);
+                this.moviments.add(aux);
+            }
+            else if (t.getPeca(tempx,tempy).getColor() == this.getColor()) break;
+            else {
+                IntPair aux = new IntPair(tempx,tempy);
+                this.moviments.add(aux);
+                break;
+            }
+            tempx++;
+            tempy--;
         }
-
-        //No es pot moure ni adalt ni avall ni dreta no esuqerra
-        if (this.getX() == desti_x || this.getY() == desti_y) return false;
-
-        //No pot saltar peces
-
+        //el mateix per la resta de diagonals
+        //...
+        return this.moviments;
     }
 }
 
+//IGNORAR A APRTI D'AQUI
+/*
+    //Si hi ha una peça aliada a la posició destí, no ens podem moure
+    Peça p_aux = t.getPeça(desti_x, desti_y);
+
+        if (p_aux != null) {
+                if (p_aux.esBlanca() && this.esBlanca()) return false;
+                if (p_aux.esNegra() && this.esNegra()) return false;
+                }
+
+                //No es pot moure ni adalt ni avall ni dreta no esuqerra
+                if (this.getX() == desti_x || this.getY() == desti_y) return false;
+
+//No pot saltar peces
+*/
