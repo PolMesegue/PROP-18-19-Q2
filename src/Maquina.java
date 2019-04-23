9da52f030ae98700447bbce9e5476ffdb6a9ffbd
Cabahamaru <jaume.riera200@gmail.com>
2019-04-23 21:22:42 +0200

    Update Maquina.java

--- src/Maquina.java
+++ src/Maquina.java
@@ -3,9 +3,21 @@ import java.util.*;
 public class Maquina extends Usuari {
 
     public Maquina(String nom){
+
         super(nom);
     }
 
+    public Jugada play(Peca[] peces_blanques, Peca[] peces_negres, boolean color) {
+        ArrayList<IntPair> posiblesMoviments;
+
+        //maquina juga amb blanques color = true;
+        if (color) {
+            int n = peces_blanques.length();
+            for (int i = 0; i < ; ++i) {
+
+            }
+        }
+    }
 
 
 }
