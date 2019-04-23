3ba8e07d194c2f9fb505a540b2759191929aa46a
Cabahamaru <jaume.riera200@gmail.com>
2019-04-23 21:47:46 +0200

    ????

---
+++ src/Jugada.java
@@ -0,0 +1,7 @@
+
+
+public class Jugada {
+    Peca peca;
+    int pos_fin_x;
+    int pos_fin_y;
+}
--- src/Maquina.java
+++ src/Maquina.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+@SuppressWarnings("Duplicates")
 public class Maquina extends Usuari {
 
     public Maquina(String nom){
@@ -8,17 +9,29 @@ public class Maquina extends Usuari {
     }
 
     public Jugada play(Peca[] peces_blanques, Peca[] peces_negres, boolean color) {
-        ArrayList<IntPair> posiblesMoviments;
+
+        //totes les jugades que pot fer un jugador
+        ArrayList<IntPair> posiblesMoviments = new ArrayList<IntPair>();
 
         //maquina juga amb blanques color = true;
         if (color) {
-            int n = peces_blanques.length();
-            for (int i = 0; i < ; ++i) {
-
+            for (int i = 0; i < peces_blanques.length; ++i) {
+                ArrayList<IntPair> moviments = peces_blanques[i].getMoviments();
+                for (int j = 0; j < peces_blanques[i].getMoviments().size(); ++j) {
+                    posiblesMoviments.add(moviments.get(j));
+                }
             }
         }
+        //maquina juga amb negres color = false
+        else if (!color) {
+            for (int i = 0; i < peces_negres.length; ++i) {
+                ArrayList<IntPair> moviments = peces_negres[i].getMoviments();
+                for (int j = 0; j < peces_negres[i].getMoviments().size(); ++j) {
+                    posiblesMoviments.add(moviments.get(j));
+                }
+            }
+        }
+        return null;
     }
-
-
 }
 
