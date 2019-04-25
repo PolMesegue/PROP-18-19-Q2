e227d6a3db9438e6e25bcfa0fba028a57286ee1e
Jaume Riera Caba <33702960+Cabahamaru@users.noreply.github.com>
2019-04-25 10:52:59 +0200

    Update Maquina.java

--- src/Maquina.java
+++ src/Maquina.java
@@ -46,7 +46,7 @@ public class Maquina extends Usuari {
     }
 
     //comprova si la peca arriba al rei en n jugades
-    private boolean backtracking(Jugada jugada, IntPair pos_rei, Tauler t, int i, int n) {
+    private boolean backtracking(Jugada jugada, Tauler t, int i, int n) {
         if (i > n) return false;
         //MIRAR SI REI ESTA EN MAT
         else if (t.white_king_in_mate()) return true;
@@ -56,15 +56,15 @@ public class Maquina extends Usuari {
                 ArrayList<IntPair> visited = new ArrayList<>();
                 IntPair actual_pos = new IntPair(t.getPeces_blanques()[i].getX(),t.getPeces_blanques()[i].getY());
                 visited.add(actual_pos);
-                for (int z = 0; z < t.getPeces_blanques()[j].moviments.size(); ++z) {
-                    if (!isvisited(t.getPeces_blanques()[j].moviments.get(z),visited)) {
-                        t.getPeces_blanques()[j].setX(t.getPeces_blanques()[j].moviments.get(z).getX());
-                        t.getPeces_blanques()[j].setY(t.getPeces_blanques()[j].moviments.get(z).getY());
+                for (int z = 0; z < t.getPeces_negres()[j].moviments.size(); ++z) {
+                    if (!isvisited(t.getPeces_negres()[j].moviments.get(z),visited)) {
+                        t.getPeces_negres()[j].setX(t.getPeces_negres()[j].moviments.get(z).getX());
+                        t.getPeces_negres()[j].setY(t.getPeces_negres()[j].moviments.get(z).getY());
                         t.actualitzar();
-                        jugada.setPeca(t.getPeces_blanques()[j]);
-                        jugada.setPos_fin_x(t.getPeces_blanques()[j].moviments.get(z).getX());
-                        jugada.setPos_fin_y(t.getPeces_blanques()[j].moviments.get(z).getY());
-                        b = backtracking(jugada,pos_rei,t,i+1,n);
+                        jugada.setPeca(t.getPeces_negres()[j]);
+                        jugada.setPos_fin_x(t.getPeces_negres()[j].moviments.get(z).getX());
+                        jugada.setPos_fin_y(t.getPeces_negres()[j].moviments.get(z).getY());
+                        b = backtracking(jugada,t,i+1,n);
                         if (b) break;
                     }
                 }
@@ -74,18 +74,18 @@ public class Maquina extends Usuari {
     }
 
     public boolean te_solucio(Peca[] peces_blanques, Peca[] peces_negres, int n) {
-
-        IntPair rei_pos = new IntPair();
+        /*IntPair rei_pos = new IntPair();
         for (int i = 0; i < peces_negres.length; ++i) {
             if (peces_negres[i] instanceof Rei) {
                 rei_pos.setX(peces_negres[i].getX());
                 rei_pos.setY(peces_negres[i].getY());
             }
         }
+        */
         Jugada jugada = new Jugada();
         boolean b = false;
         Tauler t_temp = new Tauler(peces_blanques,peces_negres);
-        return backtracking(jugada,rei_pos,t_temp,0,n);
+        return backtracking(jugada,t_temp,0,n);
     }
 }
 
