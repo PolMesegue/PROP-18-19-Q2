bbbd8f43208d51b7875bdd3f5f75a5bf1c303578
naki2 <joanetpont@gmail.com>
2019-05-09 18:58:15 +0200

    Create CtrlPresentacio.java

---
+++ src/presentacio/CtrlPresentacio.java
@@ -0,0 +1,25 @@
+package presentacio;
+
+import javax.swing.*;
+import domini.Ctrl_Domini;
+
+
+import java.util.Collection;
+public class CtrlPresentacio {
+
+    private Ctrl_Domini ctrldom = new Ctrl_Domini();
+
+
+
+
+
+
+    public static void main(String[] args){
+        CtrlPresentacio CtrlP = new CtrlPresentacio();
+
+        //Aqui ficar el main que obrira la primera pantalla pol
+
+
+    }
+
+}
