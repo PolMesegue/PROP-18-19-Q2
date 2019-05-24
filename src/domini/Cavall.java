0e1433e2f74928b3cb0848a30c3f3bc081a858f5
Jaume Riera Caba <33702960+Cabahamaru@users.noreply.github.com>
2019-05-24 22:44:43 +0200

    Update Cavall.java

--- src/domini/Cavall.java
+++ src/domini/Cavall.java
@@ -19,7 +19,7 @@ public class Cavall extends Peca{
         Tauler t = new Tauler(peces_blanques,peces_negres);
         this.moviments.clear();
         int posx[] = {this.getX() - 2, this.getX() - 1, this.getX() + 1, this.getX() + 2, this.getX() - 2, this.getX() - 1, this.getX() + 1, this.getX() + 2};
-        int posy[] = {this.getY() - 1, this.getY() - 2, this.getY() - 2, this.getY() - 1, this.getY() + 1, this.getY() + 2, this.getY() + 2, this.getY() - 1};
+        int posy[] = {this.getY() - 1, this.getY() - 2, this.getY() - 2, this.getY() - 1, this.getY() + 1, this.getY() + 2, this.getY() + 2, this.getY() + 1};
         for (int i = 0; i < 8; ++i) {
             if (posx[i] >= 0 && posx[i] < 8 && posy[i] >= 0 && posy[i] < 8) {
                 if (t.getPeca(posx[i],posy[i]) == null || t.getPeca(posx[i],posy[i]).getColor() != this.getColor()) {
