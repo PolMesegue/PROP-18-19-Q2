d7c13215173b8903907757d6ec9374449db09be6
naki2 <joanetpont@gmail.com>
2019-05-25 15:29:12 +0200

    Ranking

---
+++ src/domini/Ranking.java
@@ -0,0 +1,21 @@
+package domini;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.*;
+
+public class Ranking {
+    Map<String,Integer> map = new HashMap<String,Integer>();
+
+
+
+    public void addUsuariGuanyador(String nom){
+        if(map.get(nom)==null){
+
+        }
+
+
+    }
+
+}
